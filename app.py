b7e4925a1c16a46d5e074a3cf142e4c8c848b8df
Rishi <dracorishi@gmail.com>
2017-02-13 00:37:56 -0800

    adding user query

--- app.py
+++ app.py
@@ -51,5 +51,9 @@ def checkin():
 if __name__ == '__main__':
   db.create_all()
   db.session.commit()
+
+  user = User('test', 'email', 'testloc')
+  db.session.add(user)
+  db.session.commit()
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port, debug=True)

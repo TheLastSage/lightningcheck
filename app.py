b0a97524b88acf3cafb8f3b05e001a704c7628f4
Rishi <dracorishi@gmail.com>
2017-02-13 01:05:54 -0800

    added repr for User and fixed import deprecation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 

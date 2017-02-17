d0cd005a167661382d590d1479e86fcf1a9a5402
Rishi <dracorishi@gmail.com>
2017-02-17 15:49:09 -0800

    bugfixing here check

--- app.py
+++ app.py
@@ -208,9 +208,9 @@ def week_check(number):
       person = weekly[email]
       if person["time2"] is not None:
         person_first = person["time1"]
-        if time1 + DIFF > person_first and time1 - DIFF < person_first:
+        if time1 is not None and time1 + DIFF > person_first and time1 - DIFF < person_first:
           person_second = person["time2"]
-          if time2 + DIFF > person_second and time2 - DIFF < person_second:
+          if time2 is not None and time2 + DIFF > person_second and time2 - DIFF < person_second:
             temp_here.append(email)
 
     for email in PEOPLE:
